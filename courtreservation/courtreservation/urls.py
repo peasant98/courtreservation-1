bfe6f6270d33ce649edd9d39dba9298f24576bbf
Ayush Khanal <vegetablebat1@gmail.com>
2018-04-16 19:29:47 -0600

    made necessary changes to make second app ready for development

--- courtreservation/courtreservation/urls.py
+++ courtreservation/courtreservation/urls.py
@@ -21,4 +21,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('courts1.urls')),
+    url(r'^reserver/', include('reserveScreen.urls')),
 ]
