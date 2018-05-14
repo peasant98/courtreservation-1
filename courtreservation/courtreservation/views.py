3bbfb04fe16566069568876c02d2852c725827f1
jptboy <vegetablebat1@gmail.com>
2018-05-13 21:26:44 -0600

    made updates go to all clients

--- courtreservation/courtreservation/views.py
+++ courtreservation/courtreservation/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from reserveScreen.models import Court
+from courts1.models import Loaded_Team,Team,Player
 # Create your views here.
 def sendThem(request):
     return render(request, 'sendThem.html')
+def nuke(request):
+    Loaded_Team.objects.all().delete()
+    Team.objects.all().delete()
+    Player.objects.all().delete()
+
+    for courts in Court.objects.all():
+        courts.courtreserved=False
+        courts.save()
+    
+    return HttpResponse('Boom')
--- courtreservation/reserveScreen/themethod.py
+++ courtreservation/reserveScreen/themethod.py
@@ -1,52 +1,54 @@
 from reserveScreen.models import Court
 from courts1.models import Loaded_Team,Team,Player
-teams = Loaded_Team.objects.all()#team list#courts list of raw courts that havent been decided 
-finalholder=[]
+import time
+
 def assigner():
-    courtQueue= list(Court.objects.all())
-    for i in range(0,len(finalholder)):
-        finalholder.pop()
-
-    '''
-    for courts in courtQueue:
-        if(courts.courtreserved==True):#removing the reserved courts from the queue
-            courtQueue.pop(courts)
-    '''
-
-    courtQueue=[x for x in courtQueue if x.courtreserved==False]#this is god incarnated in programming
-
-    if len(courtQueue)<1 or (len(teams)!=2):
-        print("There are no courts available!")# error message if there are no courts avaiable or somehow there are more than two teams
-        Loaded_Team.objects.all().delete()##we will have to remove THIS, this is just to prevent me hassle
-        return 0
-
-    else:
-        randCourt = courtQueue[len(courtQueue)-1]#setting randcourt to last thing in the queue of courtQueueerved courts
-        courtQueue[len(courtQueue)-1].courtreserved=True
-        finalholder.append(courtQueue[len(courtQueue)-1].courtNum)
-
+    for teamers in Loaded_Team.objects.all():
+       teamers.refresh_from_db()
+       teamers.save() 
+    teams = list(Loaded_Team.objects.all())#team list#courts list of raw courts that havent been decided
+    teams = [x for x in teams if x.isplaying==False]
+    
+    for courters in Court.objects.all():
+       courters.refresh_from_db()
+       courters.save()
+    allcourts=list(Court.objects.all())
+    courts=[x for x in allcourts if x.courtreserved==False]
+    
+    
+    checker=0
+    
+    if len(teams)==0 and len(Team.objects.all())==0 and len(courts)>0:
+        checker=1
+    
+    while len(teams)!=2 and checker==0 and len(courts)>0:
+        time.sleep(10)
+        print("The number of loaded teams is " + str(len(teams)))
+        for teamers in Loaded_Team.objects.all():
+            teamers.refresh_from_db()
+            teamers.save()
+        for courters in Court.objects.all():
+            courters.refresh_from_db()
+            courters.save()
+        allcourts=list(Court.objects.all())
+        courts=[x for x in allcourts if x.courtreserved==False]
+        teams = list(Loaded_Team.objects.all())#team list#courts list of raw courts that havent been decided
+        teams = [x for x in teams if x.isplaying==False]
+        assigner()
+    
+    if (len(teams)==2 and len(courts)>0):# or (len(teams)==0 and len(courts)>0):
+        thecourt=courts[-1].courtNum
         for courts in Court.objects.all():
-            if courts.courtNum==courtQueue[len(courtQueue)-1].courtNum:
+            if courts.courtNum==thecourt:
                 courts.courtreserved=True
+                actualcourt=courts
                 courts.save()
-
-
-        courtQueue.pop()#removes the last item in the queue
-
-        for team in teams:
-            team.court_id=randCourt#setting the teams court to the last court in the queue of courts
-
-        Loaded_Team.objects.all().delete()
-        print("The teams have been assigned to play on "+ "Court"+str(randCourt.courtNum) + ".")
-        if len(finalholder)>0:
-            return finalholder[0]
-            print(str(finalholder[0]))
+        for team in Loaded_Team.objects.all():
+            if team in teams:
+                team.court_id=actualcourt
+                team.isplaying=True
+                team.save()
+        #sfixreserved()
+        return thecourt
     
-    
-    
-    print("List of unreserved courts is this long:" + str(len(courtQueue)))
-    for courts in courtQueue:
-        print(str(courts))
-
-
 
